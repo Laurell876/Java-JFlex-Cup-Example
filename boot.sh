8830d8a353a49070fa744ed10db1d3fbffac1060
laurell <you@example.com>
2022-03-13 15:31:48 -0500

    Feat, change boot and add test file

--- boot.sh
+++ boot.sh
@@ -1,16 +1,12 @@
-#!/bin/bash
 
-#compile
-jflex-1.6.1/bin/jflex lcalc.flex;
-java -cp .:java-cup-11b.jar java_cup.Main  < ycalc.cup;
-javac -cp .:java-cup-11b.jar Main.java;
-
-#run
-java -cp .:java-cup-11b-runtime.jar Main test.txt;
+# NEW SCRIPT - must have jflex and jcup setup locally
+jflex lcalc.flex;
+java java_cup.Main ycalc.cup;
+javac Main.java
+java Main project-test.txt
 
 #clean
 rm Lexer.java;
 rm parser.java;
 rm sym.java;
 rm *.class;
-
